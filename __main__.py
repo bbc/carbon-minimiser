e5bb35bd1b9d0bb6437950e411d265ffcefb015a
Iain McClenaghan <iain.mcclenaghan@bbc.co.uk>
2021-11-26 16:31:56 +0000

    Start caching in new thread

--- __main__.py
+++ __main__.py
@@ -1,15 +1,23 @@
 from minimiser_api.app import app
 import argparse
 from cache.cache import Cache
+import threading
+
+def cache():
+    cache = Cache(60*30)  # Update cache every 30 mins
+    cache.start_caching()
 
 def main(port):
-    Cache(60*30)  # Update cache every 30 mins
     app.run(host='0.0.0.0', port=port)
 
-
 parser = argparse.ArgumentParser()
+parser.add_argument('-c', action='store_true')
 parser.add_argument('-p')
 args = parser.parse_args()
+
+if args.c:
+    thread = threading.Thread(target=cache, daemon=True)
+    thread.start()
 if args.p:
     port = int(args.p)
 else:
